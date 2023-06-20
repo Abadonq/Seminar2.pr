a3621f5ee06db4eb17c640394e524925bd486144
Кузилин Юра <yurakuzilin@gmaio.com>
2023-06-20 18:40:05 +0300

    10 task complete

---
+++ Task10/Program.cs
@@ -0,0 +1,7 @@
+﻿ Console.WriteLine("input your number(100-999)");
+           int number = Convert.ToInt32(Console.ReadLine());
+            int num2 = number / 10 %10;
+            Console.WriteLine(num2);
+
+           
+ 
