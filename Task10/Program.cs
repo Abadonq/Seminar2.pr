3e3999bb04818669fda309dd0dbad010d0d43ceb
Кузилин Юра <yurakuzilin@gmaio.com>
2023-06-20 19:50:30 +0300

    task 15 complete

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,7 +1,18 @@
-﻿ Console.WriteLine("input your number(100-999)");
-           int number = Convert.ToInt32(Console.ReadLine());
-            int num2 = number / 10 %10;
-            Console.WriteLine(num2);
+﻿ int GetNum()
+ {
+    Console.WriteLine("input your number(100-999)");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+ }
+int logic()
+ {       
+            int num2 = GetNum() / 10 %10;
+            return num2;
+ }
 
-           
+
+int result = logic();
+Console.WriteLine(result);
+ 
+ 
  
---
+++ Task15/Program.cs
@@ -0,0 +1,44 @@
+﻿int GetNum()
+{
+    Console.WriteLine("Введите число от 1 до 7");
+    int user_input = Convert.ToInt32(Console.ReadLine());
+    return user_input;
+}
+
+switch(GetNum())
+{
+case 1:
+ Console.WriteLine("нет");
+break;
+
+case 2:
+ Console.WriteLine("нет");
+break;
+
+case 3:
+ Console.WriteLine("нет");
+break;
+
+case 4:
+ Console.WriteLine("нет");
+break;
+
+case 5:
+ Console.WriteLine("нет");
+break;
+
+case 6:
+ Console.WriteLine("да");
+break;
+
+case 7:
+Console.WriteLine("да");
+break;
+
+default:
+ Console.WriteLine("число не правильное");
+break;
+}
+
+
+
